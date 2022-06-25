f9a0dde0541981dabaa121325b3354757e10543b
Daniel <stickbugdanny@gmail.com>
2022-06-25 19:31:45 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module group.mathinfo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens group.mathinfo to javafx.fxml;
+    exports group.mathinfo;
+}
