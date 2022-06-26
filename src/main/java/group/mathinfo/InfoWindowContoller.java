1f73dcba9fc2d8f54d6e1d389242d86a550f3f43
Данило Козак <danylo.kozak@ukma.edu.ua>
2022-06-27 01:22:12 +0300

    Added info window

---
+++ src/main/java/group/mathinfo/InfoWindowContoller.java
@@ -0,0 +1,29 @@
+package group.mathinfo;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class InfoWindowContoller implements Initializable {
+
+    @FXML
+    private Label functionNameLabel;
+
+    @FXML
+    private TextArea functionInfoTextArea;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    public void setContents(String name, String info)
+    {
+        functionNameLabel.setText(name);
+        functionInfoTextArea.setText(info);
+    }
+}
